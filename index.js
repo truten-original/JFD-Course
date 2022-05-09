9c7f3a60e0916eb1659c2a6f16695457ee8cc616
Roman Trutnev <trutenoriginal@gmail.com>
2022-05-09 21:23:17 +0300

    task 2

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-let health = prompt('Введите число параметра "здоровье" для персонажа')
-console.log(health)
-if ( health <= 0 || health === false) {
-  
-   alert('Параметр "здоровье" должен быть больше нуля!')
-} else {
-    // console.log(health) // Health не определенно, так как написано с ошибкой 
-//    alert(`Параметр "здоровье" равен ${Health}`);
-   alert(`Параметр "здоровье" равен ${health}`);
-} 
+const temperatureInCelsius = prompt('Введите температуру в градусах Цельсия');
+console.log(typeof(temperatureInCelsius)) // тип данных string при строгом сравнении 0 !== '0'
+if (temperatureInCelsius === 0) {
+   alert('0 градусов по Цельсию - это температура замерзания воды')
+} else if (temperatureInCelsius > 0) {
+   alert('Для замерзания воды температура должна быть 0 градусов по Цельсию либо ниже');
+}
+// с помощью chrome devtools ставим breakpoint в этом месте и анализируем название переменных ниже, видем ошибку и исправляем
+// const temperatureInFahrenheit = (temperatureIncelsius) * 9 / 5 + 32;
+const temperatureInFahrenheit = (temperatureInCelsius) * 9 / 5 + 32; // как я понимаю происходит не явное преобразование строки в число и все отрабатывает корректно
+
+// alert(`%{temperatureInCelsius} градусов по Цельсию - это %{temperatureInFahrenheit} по 
+// Фаренгейту.`); // неправильно переданы переменную в в строку, следует заменить "%" на "$"
+alert(`${temperatureInCelsius} градусов по Цельсию - это ${temperatureInFahrenheit} по 
+Фаренгейту.`);

b9743c3403e9f466f9d4ceb72fa13f873ee979e1
Roman Trutnev <trutenoriginal@gmail.com>
2022-05-09 22:09:33 +0300

    task3

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-const temperatureInCelsius = prompt('Введите температуру в градусах Цельсия');
-console.log(typeof(temperatureInCelsius)) // тип данных string при строгом сравнении 0 !== '0'
-if (temperatureInCelsius === 0) {
-   alert('0 градусов по Цельсию - это температура замерзания воды')
-} else if (temperatureInCelsius > 0) {
-   alert('Для замерзания воды температура должна быть 0 градусов по Цельсию либо ниже');
+const salaryOfJuniorDeveloper = 500;
+const numberOfJuniorDevelopers = 3;
+let taxPercentage = 13;
+let totalJuniorDevelopersSalary = 0; // присваиваем значение равное 0
+                
+for (let i = 0; i < numberOfJuniorDevelopers; i += 1) {
+   const salaryWithTax = salaryOfJuniorDeveloper-(salaryOfJuniorDeveloper*taxPercentage/100);
+   console.log(salaryWithTax, totalJuniorDevelopersSalary) // salaryWithTax считается корректно и возвращает число, а totalJuniorDevelopersSalary возвращает udefined, соответственно число + underfined = NaN
+   totalJuniorDevelopersSalary += salaryWithTax;
+  
 }
-// с помощью chrome devtools ставим breakpoint в этом месте и анализируем название переменных ниже, видем ошибку и исправляем
-// const temperatureInFahrenheit = (temperatureIncelsius) * 9 / 5 + 32;
-const temperatureInFahrenheit = (temperatureInCelsius) * 9 / 5 + 32; // как я понимаю происходит не явное преобразование строки в число и все отрабатывает корректно
-
-// alert(`%{temperatureInCelsius} градусов по Цельсию - это %{temperatureInFahrenheit} по 
-// Фаренгейту.`); // неправильно переданы переменную в в строку, следует заменить "%" на "$"
-alert(`${temperatureInCelsius} градусов по Цельсию - это ${temperatureInFahrenheit} по 
-Фаренгейту.`);
+console.log('totalJuniorDevelopersSalary', totalJuniorDevelopersSalary);

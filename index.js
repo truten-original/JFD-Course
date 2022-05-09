ff52544a432b9ba6f39f38f534eccab9fefc0990
Roman Trutnev <trutenoriginal@gmail.com>
2022-05-09 20:41:33 +0300

    homework done

---
+++ index.js
@@ -0,0 +1,10 @@
+let health = prompt('Введите число параметра "здоровье" для персонажа')
+console.log(health)
+if ( health <= 0 || health === false) {
+  
+   alert('Параметр "здоровье" должен быть больше нуля!')
+} else {
+    // console.log(health) // Health не определенно, так как написано с ошибкой 
+//    alert(`Параметр "здоровье" равен ${Health}`);
+   alert(`Параметр "здоровье" равен ${health}`);
+} 

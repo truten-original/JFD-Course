b800a0b1cf19846f292c1003b52d2aa98493e5dc
Roman Trutnev <trutenoriginal@gmail.com>
2022-05-10 18:48:37 +0300

    first version

---
+++ index.js
@@ -0,0 +1,23 @@
+getSumOfNumbers = (number, type) => {
+    let sum = 0
+    if (type === 'even') {
+        for (i = 1; i <= number; i++) {
+            if (i % 2 === 0) {
+                sum += i
+            }
+        }
+    } else if (type === 'odd') {
+        for (i = 1; i <= number; i++) {
+            if (i % 2 !== 0) {
+                sum += i
+            }
+        }
+    } else if (type === '') {
+        for (i = 1; i <= number; i++) {
+            sum += i
+        }
+    }
+    return sum
+}
+
+

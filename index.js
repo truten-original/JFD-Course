97483ecc04e7d1ae83f38c983f8aceb6c276c14c
Roman Trutnev <trutenoriginal@gmail.com>
2022-05-10 16:27:03 +0300

    changed version

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let health = prompt('Введите число параметра "здоровье" для персонажа')
 console.log(health)
-if ( health <= 0 || health === false) {
+if ( health <= 0 || !health) {
   
    alert('Параметр "здоровье" должен быть больше нуля!')
 } else {

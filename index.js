d26f9016695eab372c73a9c9f9e5ccd68f21fc16
Roman Trutnev <trutenoriginal@gmail.com>
2022-05-11 20:43:25 +0300

    final task4

--- index.js
+++ index.js
@@ -5,20 +5,18 @@ let sum = 0
 // Через цикл for
 for (let i = 0; i < numbers.length; i += 1) {
     let num
-    num = numbers[i] ** 3
-    sum += num
+    sum += num ** 3
     console.log(sum, numbers[i])
-    
 
-} 
+
+}
 console.log(sum) // 1158411
 
 // Через цикл for of
 sum = 0
-for (let num of numbers){
-    num = num ** 3
-    sum += num
+for (let num of numbers) {
+    sum += num ** 3
     // видим что значения num не соответствуют значениям исходного массива, из-за того что во время использования первого цикла они были возведены в третью степень, инициализируем переменную num и будем присваивать ей значение number[i] ** 3, тем самым избежим мутирования первоначального массива
     console.log(sum, num)
-} 
+}
 console.log(sum) // 1003904306910622800

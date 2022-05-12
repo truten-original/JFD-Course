1c241dcce00260b715d4e366938d30f90bc1e6e8
Roman Trutnev <trutenoriginal@gmail.com>
2022-05-12 18:44:39 +0300

    module 7 task1

---
+++ module7/index.js
@@ -0,0 +1,14 @@
+const getName1 = function (name) {
+    return `Имя равно ${name}`
+}
+const getName2 = (name) => {
+    return `Имя равно ${name}`
+
+}
+function getName3(name) {
+    return `Имя равно ${name}`
+}
+
+console.log(getName1('Roman'))
+console.log(getName2('Oksana'))
+console.log(getName3('Misha'))

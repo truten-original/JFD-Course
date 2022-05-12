70e446e4c26e1d0379b66e6fc29dc617b186aaa6
Roman Trutnev <trutenoriginal@gmail.com>
2022-05-12 18:39:19 +0300

    module 7 task 2

---
+++ module7/task 2/index.js
@@ -0,0 +1,37 @@
+// getSumOfNumbers = (number = 10, type) => {
+//     let sum = 0
+//     if (type === 'even') {
+//         for (i = 1; i <= number; i++) {
+//             if (i % 2 === 0) {
+//                 sum += i
+//             }
+//         }
+//     } else if (type === 'odd') {
+//         for (i = 1; i <= number; i++) {
+//             if (i % 2 !== 0) {
+//                 sum += i
+//             }
+//         }
+//     } else if (type === '') {
+//         for (i = 1; i <= number; i++) {
+//             sum += i
+//         }
+//     }
+//     return sum
+// }
+
+getSumOfNumbers = (number, type = 'odd') => {
+    let sum = 0
+    for (i = 1; i <= number; i++) {
+        const isEven = i % 2 === 0
+        const isOdd = !isEven
+        if (!type) {
+            sum += i
+        } else if (type === 'odd' && isOdd) {
+            sum += i
+        } else if (type === 'even' && isEven) {
+            sum += i
+        }
+    }
+    return sum
+}

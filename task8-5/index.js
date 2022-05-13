76b25d85571338fb035fee2ae51c9fa646cb8e46
Roman Trutnev <trutenoriginal@gmail.com>
2022-05-13 20:19:30 +0300

    module8 task5

--- task8-5/index.js
+++ task8-5/index.js
@@ -1,14 +1,16 @@
 const clientsEstimations = []
 
 function askClientToGiveEstimation() {
-    let estimation = prompt('Как вы оцениваете нашу кофейню от 1 до 10?')
+    let estimation = prompt('Как вы оцениваете нашу кофейню от 1 до 10?', 10)
     if (typeof (estimation === 'number')) {
         clientsEstimations.push(estimation)
+    }else if(!estimation){
+        alert('пожалуйста используйте числа от 1 до 10 для оценки')
     }
 }
 for (let i = 0; i < 5; i++){
     askClientToGiveEstimation()
 }
-const goodEstimations = clientsEstimations.filter(estimation => estimation >= 5)
+const goodEstimations = clientsEstimations.filter(estimation => estimation > 5)
 const notGoodEstimations = clientsEstimations.filter(estimation => estimation <= 5)
 alert(`Всего положительных оценок: ${goodEstimations.length}; Всего отрицательных оценок: ${notGoodEstimations.length}`)

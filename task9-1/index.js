6be2ae00544c1ad5342743773f836d58a7f8e747
Roman <trutenoriginal@gmail.com>
2022-05-17 19:54:22 +0300

    module9 task1

---
+++ task9-1/index.js
@@ -0,0 +1,19 @@
+const users = [
+    {
+        username: 'David',
+        status: 'online',
+        lastActivity: 10
+    },
+    {
+        username: 'Lucy',
+        status: 'offline',
+        lastActivity: 22
+    },
+    {
+        username: 'Bob',
+        status: 'online',
+        lastActivity: 104
+    }
+]
+const onlineUsers = users.filter(user => user.status === 'online')
+console.log(onlineUsers)

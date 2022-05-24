84972b8157ec577d8ceaaa2e606cea352283d55f
Roman Trutnev <trutenoriginal@gmail.com>
2022-05-24 22:14:51 +0300

    third version

--- task11-2/index.js
+++ task11-2/index.js
@@ -16,22 +16,15 @@ const bird = {
 function makeDomestic(isDomestic) {
     const animal = {
         isDomestic: isDomestic,
+
     }
     const objArr = Object.keys(this)
     for (let i = 0; i < objArr.length; i++) {
         animal[objArr[i]] = this[objArr[i]]
     }
-    animal.makeSound = function () { console.log(`${this.type} по имени ${this.name} говорит ${this.makeSound()}`) }
+    console.log(`${this.type} по имени ${this.name} говорит ${this.makeSound()}`)
     return animal
-
-    // const animal = {}
-    // animal.name = this.name
-    // animal.type = this.type
-    // animal.isDomestic = isDomestic
-    // animal.makeSound = function(){
-    //     console.log(`${this.type} по имени ${this.name} говорит ${this.makeSound()}`)
 }
-
 console.log(makeDomestic.bind(dog, true)())
-makeDomestic.call(bird, false)
-makeDomestic.apply(bird, [false])
+console.log(makeDomestic.call(bird, false))
+console.log(makeDomestic.apply(bird, [false]))

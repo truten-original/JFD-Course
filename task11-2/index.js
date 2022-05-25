ce3ff2a9b9853a156e1c1e76d4c8c6a26b7ee8c1
Roman Trutnev <trutenoriginal@gmail.com>
2022-05-25 18:50:07 +0300

    module 11 task 2

--- task11-2/index.js
+++ task11-2/index.js
@@ -13,15 +13,20 @@ const bird = {
         return "Чик-чирик";
     }
 }
-function makeDomestic(isDomestic) {
-    const animal = {
-        isDomestic: isDomestic,
+// function makeDomestic(isDomestic) {
+//     const animal = {
+//         isDomestic: isDomestic,
 
-    }
-    const objArr = Object.keys(this)
-    for (let i = 0; i < objArr.length; i++) {
-        animal[objArr[i]] = this[objArr[i]]
-    }
+//     }
+//     const objArr = Object.keys(this)
+//     for (let i = 0; i < objArr.length; i++) {
+//         animal[objArr[i]] = this[objArr[i]]
+//     }
+//     console.log(`${this.type} по имени ${this.name} говорит ${this.makeSound()}`)
+//     return animal
+// }
+function makeDomestic(isDomestic) {
+    const animal = { ...this, isDomestic: isDomestic }
     console.log(`${this.type} по имени ${this.name} говорит ${this.makeSound()}`)
     return animal
 }
